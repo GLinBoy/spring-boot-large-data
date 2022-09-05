81975dfbf350f728a243f43c6cde9608b8e38c90
GLinBoy <hojjatabedie@gmail.com>
2022-09-05 21:28:36 +0200

    📦 NEW: Added Coordinates dto

---
+++ src/main/kotlin/com/glinboy/test/largedata/dto/Coordinates.kt
@@ -0,0 +1,8 @@
+package com.glinboy.test.largedata.dto
+
+data class Coordinates(
+
+    val _type: String,
+    val longitude: Double,
+    val latitude: Double
+)
---
+++ src/main/kotlin/com/glinboy/test/largedata/dto/TweetNewsResponse.kt
@@ -0,0 +1,33 @@
+package com.glinboy.test.largedata.dto
+
+data class TweetNewsResponse(
+
+    val _type: String,
+    val url: String,
+    val date: String,
+    val content: String,
+    val renderedContent: String,
+    val id: Int,
+    val user: User,
+    val replyCount: Int,
+    val retweetCount: Int,
+    val likeCount: Int,
+    val quoteCount: Int,
+    val conversationId: Int,
+    val lang: String,
+    val source: String,
+    val sourceUrl: String,
+    val sourceLabel: String,
+    val outlinks: String,
+    val tcooutlinks: String,
+    val media: String,
+    val retweetedTweet: String,
+    val quotedTweet: String,
+    val inReplyToTweetId: Int,
+    val inReplyToUser: InReplyToUser,
+    val mentionedUsers: List<MentionedUsers>,
+    val coordinates: Coordinates,
+    val place: Place,
+    val hashtags: String,
+    val cashtags: String
+)
