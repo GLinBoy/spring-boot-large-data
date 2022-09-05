d24a90feed3c804e4060d7830f709e4a119bf4d7
GLinBoy <hojjatabedie@gmail.com>
2022-09-05 21:28:52 +0200

    📦 NEW: Added InReplyToUser dto

---
+++ src/main/kotlin/com/glinboy/test/largedata/dto/InReplyToUser.kt
@@ -0,0 +1,28 @@
+package com.glinboy.test.largedata.dto
+
+data class InReplyToUser(
+
+	val _type: String,
+	val username: String,
+	val id: Int,
+	val displayname: String,
+	val description: String,
+	val rawDescription: String,
+	val descriptionUrls: String,
+	val verified: String,
+	val created: String,
+	val followersCount: String,
+	val friendsCount: String,
+	val statusesCount: String,
+	val favouritesCount: String,
+	val listedCount: String,
+	val mediaCount: String,
+	val location: String,
+	val protected: String,
+	val linkUrl: String,
+	val linkTcourl: String,
+	val profileImageUrl: String,
+	val profileBannerUrl: String,
+	val label: String,
+	val url: String
+)
