a4877c62d6926a9b868aab9a218417d8c26e0307
GLinBoy <hojjatabedie@gmail.com>
2022-09-06 20:38:30 +0200

    📦 NEW: Added Place dto

---
+++ src/main/kotlin/com/glinboy/test/largedata/dto/Place.kt
@@ -0,0 +1,11 @@
+package com.glinboy.test.largedata.dto
+
+data class Place(
+
+    val _type: String,
+    val fullName: String,
+    val name: String,
+    val type: String,
+    val country: String,
+    val countryCode: String
+)
