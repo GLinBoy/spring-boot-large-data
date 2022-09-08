1b976cf9cca24d73baeb74bcb8850f8d770db714
GLinBoy <hojjatabedie@gmail.com>
2022-09-08 21:59:07 +0200

    👌 IMPROVE: Renamed typo in Place

--- src/main/kotlin/com/glinboy/test/largedata/dto/Place.kt
+++ src/main/kotlin/com/glinboy/test/largedata/dto/Place.kt
@@ -1,11 +1,13 @@
 package com.glinboy.test.largedata.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Place(
 
-    val _type: String,
+    @JsonProperty("_type") val type: String,
     val fullName: String,
     val name: String,
-    val type: String,
+    @JsonProperty("type") val placeType: String,
     val country: String,
     val countryCode: String
 )
