7b4505f2022ce54e0c4d9263131a1ec53859c2dd
GLinBoy <hojjatabedie@gmail.com>
2022-09-09 19:42:16 +0200

    👌 IMPROVE: Added DTO suffix to the Coordinates

--- src/main/kotlin/com/glinboy/test/largedata/dto/CoordinatesDTO.kt
+++ src/main/kotlin/com/glinboy/test/largedata/dto/CoordinatesDTO.kt
@@ -2,7 +2,7 @@ package com.glinboy.test.largedata.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class Coordinates(
+data class CoordinatesDTO(
 
     @JsonProperty("_type") val type: String,
     val longitude: Double,
