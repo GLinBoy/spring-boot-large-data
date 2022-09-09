a7f59234cbd62d7cf26ebd7ac141962af93976a2
GLinBoy <hojjatabedie@gmail.com>
2022-09-09 19:43:30 +0200

    👌 IMPROVE: Added DTO suffix to the Place

---
+++ src/main/kotlin/com/glinboy/test/largedata/dto/PlaceDTO.kt
@@ -0,0 +1,13 @@
+package com.glinboy.test.largedata.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class PlaceDTO(
+
+    @JsonProperty("_type") val type: String,
+    val fullName: String,
+    val name: String,
+    @JsonProperty("type") val placeType: String,
+    val country: String,
+    val countryCode: String
+)
