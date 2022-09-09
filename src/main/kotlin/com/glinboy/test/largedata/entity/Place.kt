010e1b9cf78ab501094b555058df23eadd3cbdeb
GLinBoy <hojjatabedie@gmail.com>
2022-09-09 19:55:26 +0200

    👌 IMPROVE: Added Place entity

---
+++ src/main/kotlin/com/glinboy/test/largedata/entity/Place.kt
@@ -0,0 +1,16 @@
+package com.glinboy.test.largedata.entity
+
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class Place(
+
+    @Id val id: String,
+    val type: String,
+    val fullName: String,
+    val name: String,
+    val placeType: String,
+    val country: String,
+    val countryCode: String
+)
