d01834249db2338d1726ba0e53a563a9ca2fd6c5
GLinBoy <hojjatabedie@gmail.com>
2022-09-09 19:43:44 +0200

    👌 IMPROVE: Added DTO suffix to the User

---
+++ src/main/kotlin/com/glinboy/test/largedata/dto/UserDTO.kt
@@ -0,0 +1,30 @@
+package com.glinboy.test.largedata.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class UserDTO(
+
+    @JsonProperty("_type") val type: String,
+    val username: String,
+    val id: Int,
+    val displayname: String,
+    val description: String,
+    val rawDescription: String,
+    val descriptionUrls: String,
+    val verified: Boolean,
+    val created: String,
+    val followersCount: Int,
+    val friendsCount: Int,
+    val statusesCount: Int,
+    val favouritesCount: Int,
+    val listedCount: Int,
+    val mediaCount: Int,
+    val location: String,
+    val protected: Boolean,
+    val linkUrl: String,
+    val linkTcourl: String,
+    val profileImageUrl: String,
+    val profileBannerUrl: String,
+    val label: String,
+    val url: String
+)
