07943d4aca5a6bbdd03fb859f0a72bf1d63080cd
GLinBoy <hojjatabedie@gmail.com>
2022-09-13 20:06:14 +0200

    👌 IMPROVE: Made PlaceDTO properties optional

--- src/main/kotlin/com/glinboy/test/largedata/dto/PlaceDTO.kt
+++ src/main/kotlin/com/glinboy/test/largedata/dto/PlaceDTO.kt
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class PlaceDTO(
 
-    @JsonProperty("_type") val type: String,
-    val fullName: String,
-    val name: String,
-    @JsonProperty("type") val placeType: String,
-    val country: String,
-    val countryCode: String
+    @JsonProperty("_type") val type: String?,
+    val fullName: String?,
+    val name: String?,
+    @JsonProperty("type") val placeType: String?,
+    val country: String?,
+    val countryCode: String?
 )
