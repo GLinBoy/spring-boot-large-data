c3025facccd2c0a22fc5a61b456ce3b961319734
GLinBoy <hojjatabedie@gmail.com>
2022-09-14 19:45:39 +0200

    👌 IMPROVE: Added JPA annotations to the DescriptionUrls entity

--- src/main/kotlin/com/glinboy/test/largedata/entity/DescriptionUrls.kt
+++ src/main/kotlin/com/glinboy/test/largedata/entity/DescriptionUrls.kt
@@ -1,8 +1,16 @@
 package com.glinboy.test.largedata.entity
 
+import javax.persistence.ElementCollection
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
 data class DescriptionUrls(
+
+    @Id val id: Long?,
     val text: String?,
     val url: String?,
     val tcourl: String?,
+    @ElementCollection
     val indices: List<Long>?
 )
