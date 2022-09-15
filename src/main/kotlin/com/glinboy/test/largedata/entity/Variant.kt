ec475f48d4480d5e1aaf3de7d057e7d23692d2f4
GLinBoy <hojjatabedie@gmail.com>
2022-09-15 19:57:10 +0200

    👌 IMPROVE: Added JPA annotations to Variant

--- src/main/kotlin/com/glinboy/test/largedata/entity/Variant.kt
+++ src/main/kotlin/com/glinboy/test/largedata/entity/Variant.kt
@@ -1,6 +1,11 @@
 package com.glinboy.test.largedata.entity
 
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
 data class Variant(
+    @Id val id: Long,
     val type: String?,
     val contentType: String?,
     val url: String?,
