e872396416c05dfb39b8ba129b3b6d2fdb8fc93c
GLinBoy <hojjatabedie@gmail.com>
2022-09-15 20:09:47 +0200

    👌 IMPROVE: Added JPA annotations to Media

--- src/main/kotlin/com/glinboy/test/largedata/entity/Media.kt
+++ src/main/kotlin/com/glinboy/test/largedata/entity/Media.kt
@@ -1,10 +1,16 @@
 package com.glinboy.test.largedata.entity
 
+import javax.persistence.*
+
+@Entity
 data class Media(
+    @Id val id: Long,
     val type: String?,
     val previewUrl: String?,
     val fullUrl: String?,
     val thumbnailUrl: String?,
+    @OneToMany(mappedBy = "media", fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL], orphanRemoval = true)
     val variants: List<Variant>?,
     val duration: Double?,
     val views: Long?
