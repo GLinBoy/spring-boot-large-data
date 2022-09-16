7f18d3277c68aa420f1f5cb022e5a40a3d2390a5
GLinBoy <hojjatabedie@gmail.com>
2022-09-16 18:25:02 +0200

    👌 IMPROVE: MAde all Place properties optional

--- src/main/kotlin/com/glinboy/test/largedata/entity/Place.kt
+++ src/main/kotlin/com/glinboy/test/largedata/entity/Place.kt
@@ -6,11 +6,11 @@ import javax.persistence.Id
 @Entity
 data class Place(
 
-    @Id val id: Long,
-    val type: String,
-    val fullName: String,
-    val name: String,
-    val placeType: String,
-    val country: String,
-    val countryCode: String
+    @Id val id: Long?,
+    val type: String?,
+    val fullName: String?,
+    val name: String?,
+    val placeType: String?,
+    val country: String?,
+    val countryCode: String?
 )
