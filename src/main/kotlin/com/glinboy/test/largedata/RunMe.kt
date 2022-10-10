fa17d8c51667d59296d509426cc9fcee5661f2f5
GLinBoy <hojjatabedie@gmail.com>
2022-10-10 21:16:40 +0200

    Removed ModelMapper imports and usage

--- src/main/kotlin/com/glinboy/test/largedata/RunMe.kt
+++ src/main/kotlin/com/glinboy/test/largedata/RunMe.kt
@@ -3,7 +3,6 @@ package com.glinboy.test.largedata
 import com.glinboy.test.largedata.client.DataProviderService
 import com.glinboy.test.largedata.entity.TweetNews
 import com.glinboy.test.largedata.repository.TweetNewsRepository
-import org.modelmapper.ModelMapper
 import org.slf4j.LoggerFactory
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Component
 class RunMe(val dataProviderService: DataProviderService, val tweetNewsRepository: TweetNewsRepository) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
-    private val mapper = ModelMapper()
 
     @EventListener(ApplicationReadyEvent::class)
     fun run() {
@@ -22,8 +20,6 @@ class RunMe(val dataProviderService: DataProviderService, val tweetNewsRepositor
         logger.info("---> Response size: {}", response.size)
         val dto = response[0]
         logger.info("---> DTO: {}", dto)
-        val news = mapper.map(dto, TweetNews::class.java)
-        logger.info("---> Entity: {}", news)
 //        val news = response.stream().map {
 //            logger.info("---> $it")
 //            mapper.map(it, TweetNews::class.java)
