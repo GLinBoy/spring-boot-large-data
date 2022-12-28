75b8845dbd92de434936a5072cf9922cea13df0e
GLinBoy <hojjatabedie@gmail.com>
2022-12-28 15:49:05 +0100

    👌 IMPROVE: Upgraded Kotlin to 1.7.22
    resolved #57

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.1"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.21"
-	kotlin("plugin.spring") version "1.7.21"
-	kotlin("plugin.jpa") version "1.7.21"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+	kotlin("plugin.jpa") version "1.7.22"
 	id("com.github.ben-manes.versions") version "0.44.0"
     id("org.ec4j.editorconfig") version "0.0.3"
 }
