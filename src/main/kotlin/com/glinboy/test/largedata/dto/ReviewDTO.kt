1d079bc176292565077dcc735f81b565a1a46bef
GLinBoy <hojjatabedie@gmail.com>
2023-01-19 14:20:53 +0100

    👌 IMPROVE: Added ReviewDTO for response of API

---
+++ src/main/kotlin/com/glinboy/test/largedata/dto/ReviewDTO.kt
@@ -0,0 +1,15 @@
+package com.glinboy.test.largedata.dto
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class ReviewDTO(
+    val reviewDate: String? = null,
+    val movieId: String? = null,
+    val userId: String? = null,
+    val isSpoiler: Boolean? = null,
+    val reviewText: String? = null,
+    val rating: String? = null,
+    val reviewSummary: String? = null
+)
