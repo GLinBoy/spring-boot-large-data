ae2fe7e0fbcebe98ef5dedc35d9a0cdcfac401ef
GLinBoy <hojjatabedie@gmail.com>
2023-01-19 15:10:13 +0100

    👌 IMPROVE: Disabled RunMe class to run at startup

--- src/main/kotlin/com/glinboy/test/largedata/RunMe.kt
+++ src/main/kotlin/com/glinboy/test/largedata/RunMe.kt
@@ -9,7 +9,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
-@Component
+//@Component
 class RunMe(
     private val dataProviderService: DataProviderService,
     private val reviewRepository: ReviewRepository
