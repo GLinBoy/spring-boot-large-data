433f26d9deb2ac2da519ac4c54ec5e8ffa01b5e4
GLinBoy <hojjatabedie@gmail.com>
2023-01-22 14:53:14 +0100

    👌 IMPROVE: Added data provider module to the project
    #65

---
+++ dataprovider/src/main/kotlin/com/glinboy/largedata/dataprovider/DataproviderApplication.kt
@@ -0,0 +1,11 @@
+package com.glinboy.largedata.dataprovider
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataProviderApplication
+
+fun main(args: Array<String>) {
+	runApplication<DataProviderApplication>(*args)
+}
---
+++ dataprovider/src/test/kotlin/com/glinboy/largedata/dataprovider/DataproviderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.largedata.dataprovider
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataProviderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
