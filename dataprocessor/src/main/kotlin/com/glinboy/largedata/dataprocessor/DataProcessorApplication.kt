af30f31bfda320008ce3a0d8029fc3c7b4a9b977
GLinBoy <hojjatabedie@gmail.com>
2023-01-22 14:56:35 +0100

    👌 IMPROVE: Added data processor module to the project
    #66

---
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/DataProcessorApplication.kt
@@ -0,0 +1,11 @@
+package com.glinboy.largedata.dataprocessor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataProcessorApplication
+
+fun main(args: Array<String>) {
+	runApplication<DataProcessorApplication>(*args)
+}
---
+++ dataprocessor/src/test/kotlin/com/glinboy/largedata/dataprocessor/DataProcessorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.largedata.dataprocessor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataProcessorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
