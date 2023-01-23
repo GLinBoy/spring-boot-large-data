0e25785bdedc6b8b61daa87b061dd8b38cdd7037
GLinBoy <hojjatabedie@gmail.com>
2023-01-23 14:33:24 +0100

    👌 IMPROVE: Moved ReviewDTO to the Shared module

--- shared/src/main/kotlin/com/glinboy/largedata/shared/dto/ReviewDTO.kt
+++ shared/src/main/kotlin/com/glinboy/largedata/shared/dto/ReviewDTO.kt
@@ -1,4 +1,4 @@
-package com.glinboy.largedata.dataprocessor.dto
+package com.glinboy.largedata.shared.dto
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
