d42e339a66e6183b13d0e8c361d5a5d0cb0a0e47
GLinBoy <hojjatabedie@gmail.com>
2023-01-23 14:32:27 +0100

    👌 IMPROVE: Removed unnecessary library from the Shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,10 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.2"
-	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "com.glinboy.largedata"
@@ -16,10 +13,7 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.1")
 }
 
 tasks.withType<KotlinCompile> {
