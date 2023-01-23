507d8413f0f4c63a2e0697bc23ff38e740596e19
GLinBoy <hojjatabedie@gmail.com>
2023-01-23 14:31:39 +0100

    👌 IMPROVE: Added the Shared module to the provider

--- dataprovider/build.gradle.kts
+++ dataprovider/build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 }
 
 dependencies {
+    api(project(":shared"))
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
