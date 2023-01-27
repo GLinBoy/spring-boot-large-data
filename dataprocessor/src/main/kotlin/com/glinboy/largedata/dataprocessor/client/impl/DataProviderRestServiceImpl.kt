81755ff94f6d64517f5751c94a2979f080589bbb
GLinBoy <hojjatabedie@gmail.com>
2023-01-27 16:44:23 +0100

    👌 IMPROVE: Updated data provider Impl. methods

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/client/impl/DataProviderRestServiceImpl.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/client/impl/DataProviderRestServiceImpl.kt
@@ -1,12 +1,16 @@
 package com.glinboy.largedata.dataprocessor.client.impl
 
-import com.glinboy.largedata.dataprocessor.client.DataProviderService
+import com.glinboy.largedata.dataprocessor.client.DataProviderServiceApi
 import com.glinboy.largedata.shared.dto.ReviewDTO
 import org.springframework.stereotype.Service
 
 @Service
-class DataProviderRestServiceImpl: DataProviderService {
-    override fun invokeApiProvider(): List<ReviewDTO> {
+class DataProviderRestServiceImpl: DataProviderServiceApi {
+    override fun getSampleData(): List<ReviewDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllData(): List<ReviewDTO> {
         TODO("Not yet implemented")
     }
 }
