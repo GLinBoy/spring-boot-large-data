52fd0dafcf449136d6efdda96a237396d8df9108
GLinBoy <hojjatabedie@gmail.com>
2023-01-27 16:50:31 +0100

    👌 IMPROVE: Added a logger to the DataProviderRestServiceImpl

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/client/impl/DataProviderRestServiceImpl.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/client/impl/DataProviderRestServiceImpl.kt
@@ -2,10 +2,14 @@ package com.glinboy.largedata.dataprocessor.client.impl
 
 import com.glinboy.largedata.dataprocessor.client.DataProviderServiceApi
 import com.glinboy.largedata.shared.dto.ReviewDTO
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class DataProviderRestServiceImpl: DataProviderServiceApi {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
     override fun getSampleData(): List<ReviewDTO> {
         TODO("Not yet implemented")
     }
