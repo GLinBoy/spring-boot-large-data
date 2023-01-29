d256bf68cb65e6609c2a6ca0dd09cc08f14a805d
GLinBoy <hojjatabedie@gmail.com>
2023-01-29 19:38:54 +0100

    👌 IMPROVE: Added SaveProcess structure to save reviews into DB

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/SaveProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/SaveProcess.kt
@@ -4,5 +4,22 @@ import com.glinboy.largedata.dataprocessor.entity.Review
 import com.glinboy.largedata.dataprocessor.repository.ReviewRepository
 import com.glinboy.largedata.shared.dto.ReviewDTO
 
-class SaveProcess() {
+class SaveProcess(private val reviewRepository: ReviewRepository) : AbstractProcess<List<ReviewDTO>>() {
+
+    override fun job(reviewDTOs: List<ReviewDTO>) {
+        reviewRepository.saveAll(
+            reviewDTOs.map {
+                Review(
+                    null,
+                    it.reviewDate,
+                    it.movieId,
+                    it.userId,
+                    it.isSpoiler,
+                    it.reviewText,
+                    it.rating,
+                    it.reviewSummary
+                )
+            }
+        )
+    }
 }
