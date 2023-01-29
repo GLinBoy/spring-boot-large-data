b74a5852f5c574bc948c8743482b2fd3f667fd16
GLinBoy <hojjatabedie@gmail.com>
2023-01-29 19:37:45 +0100

    📦 NEW: Added a save processor to save data into DB

---
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/SaveProcess.kt
@@ -0,0 +1,8 @@
+package com.glinboy.largedata.dataprocessor.service.processor
+
+import com.glinboy.largedata.dataprocessor.entity.Review
+import com.glinboy.largedata.dataprocessor.repository.ReviewRepository
+import com.glinboy.largedata.shared.dto.ReviewDTO
+
+class SaveProcess() {
+}
