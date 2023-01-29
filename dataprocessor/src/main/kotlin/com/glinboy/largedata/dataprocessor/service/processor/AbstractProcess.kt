35f080bc13d6e50b6a0100c1dc71b13c8ba2b881
GLinBoy <hojjatabedie@gmail.com>
2023-01-29 19:36:26 +0100

    👌 IMPROVE: Added AbstractProcess structure

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/AbstractProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/AbstractProcess.kt
@@ -3,4 +3,16 @@ package com.glinboy.largedata.dataprocessor.service.processor
 import java.util.*
 
 abstract class AbstractProcess<T> {
+    private lateinit var next: AbstractProcess<T>
+
+    fun setNext(process: AbstractProcess<T>) {
+        this.next = process
+    }
+
+    fun process(t: T) {
+        this.job(t)
+        Optional.ofNullable(next)
+            .ifPresent { it.process(t) }
+    }
+    abstract fun job(t: T)
 }
