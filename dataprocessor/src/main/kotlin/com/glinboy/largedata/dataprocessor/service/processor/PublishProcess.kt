60bd1aeaa8ea5965481b97f08183c2ae2f375739
GLinBoy <hojjatabedie@gmail.com>
2023-01-30 11:47:10 +0100

    👌 IMPROVE: Enhanced code style on PublishProcess

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Component
 class PublishProcess(private val kafkaTemplate: KafkaTemplate<Object, ReviewDTO>):
     AbstractProcess<List<ReviewDTO>>() {
 
-
     @Value("\${application.kafka-topic}")
     lateinit var topic: String
+
     override fun job(reviewDTOs: List<ReviewDTO>) {
         reviewDTOs.map {
             kafkaTemplate.send(topic, it)
