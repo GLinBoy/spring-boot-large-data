20a9d104ac69d4b68585ea9d196137350a9bba1a
GLinBoy <hojjatabedie@gmail.com>
2023-01-30 11:20:49 +0100

    👌 IMPROVE: Replaced topic name with property value

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
@@ -1,6 +1,7 @@
 package com.glinboy.largedata.dataprocessor.service.processor
 
 import com.glinboy.largedata.shared.dto.ReviewDTO
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
@@ -8,9 +9,12 @@ import org.springframework.stereotype.Component
 class PublishProcess(private val kafkaTemplate: KafkaTemplate<Object, ReviewDTO>):
     AbstractProcess<List<ReviewDTO>>() {
 
+
+    @Value("\${application.kafka-topic}")
+    lateinit var topic: String
     override fun job(reviewDTOs: List<ReviewDTO>) {
         reviewDTOs.map {
-            kafkaTemplate.send("REVIEW-TOPIC", it)
+            kafkaTemplate.send(topic, it)
         }
     }
 }
