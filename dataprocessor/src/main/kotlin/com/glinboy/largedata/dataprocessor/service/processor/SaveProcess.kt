80a286401fab15d33af665065f0e433df2cd8869
GLinBoy <hojjatabedie@gmail.com>
2023-01-30 11:11:20 +0100

    👌 IMPROVE: Enabled SaveProcess to load by spring

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/SaveProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/SaveProcess.kt
@@ -3,7 +3,9 @@ package com.glinboy.largedata.dataprocessor.service.processor
 import com.glinboy.largedata.dataprocessor.entity.Review
 import com.glinboy.largedata.dataprocessor.repository.ReviewRepository
 import com.glinboy.largedata.shared.dto.ReviewDTO
+import org.springframework.stereotype.Component
 
+@Component
 class SaveProcess(private val reviewRepository: ReviewRepository) : AbstractProcess<List<ReviewDTO>>() {
 
     override fun job(reviewDTOs: List<ReviewDTO>) {
