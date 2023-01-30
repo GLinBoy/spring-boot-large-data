732207e33b63ec8b4528eeaaa35f2430a095060d
GLinBoy <hojjatabedie@gmail.com>
2023-01-30 11:10:53 +0100

    👌 IMPROVE: Enabled PublishProcess to load by spring

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
@@ -2,7 +2,9 @@ package com.glinboy.largedata.dataprocessor.service.processor
 
 import com.glinboy.largedata.shared.dto.ReviewDTO
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
 
+@Component
 class PublishProcess(private val kafkaTemplate: KafkaTemplate<Object, ReviewDTO>):
     AbstractProcess<List<ReviewDTO>>() {
 
