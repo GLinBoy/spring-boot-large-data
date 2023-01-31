b9874b3c4bc509cc554533a46f884f4f01afd1b4
GLinBoy <hojjatabedie@gmail.com>
2023-01-31 11:04:55 +0100

    👌 IMPROVE: Added delete process to the ReviewHandler

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/ReviewHandler.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/ReviewHandler.kt
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Component
 
 @Component
 class ReviewHandler(
+    private val deleteProcess: DeleteProcess,
     private val saveProcess: SaveProcess,
     private val publishProcess: PublishProcess
 ) {
     fun getReviewHandlerChain(): AbstractProcess<List<ReviewDTO>> {
+        deleteProcess.setNext(saveProcess)
         saveProcess.setNext(publishProcess)
-        return saveProcess
+        return deleteProcess
     }
 }
