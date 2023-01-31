1f63105d110d4e366a3c1ab2ccf2cd795f5e6a01
GLinBoy <hojjatabedie@gmail.com>
2023-01-31 10:20:10 +0100

    👌 IMPROVE: Updated sample reviews method

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/impl/ReviewsServiceImpl.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/impl/ReviewsServiceImpl.kt
@@ -2,12 +2,17 @@ package com.glinboy.largedata.dataprocessor.service.impl
 
 import com.glinboy.largedata.dataprocessor.client.DataProviderServiceApi
 import com.glinboy.largedata.dataprocessor.service.ReviewsServiceApi
+import com.glinboy.largedata.dataprocessor.service.processor.ReviewHandler
 import org.springframework.stereotype.Service
 
 @Service
-class ReviewsServiceImpl(private val dataProviderServiceApi: DataProviderServiceApi): ReviewsServiceApi {
+class ReviewsServiceImpl(
+    private val dataProviderServiceApi: DataProviderServiceApi,
+    private val reviewHandler: ReviewHandler
+): ReviewsServiceApi {
     override fun fetchSampleReviews() {
-        dataProviderServiceApi.getSampleData()
+        val reviewDTOs = dataProviderServiceApi.getSampleData()
+        reviewHandler.getReviewHandlerChain().process(reviewDTOs)
     }
 
     override fun fetchAllData() {
