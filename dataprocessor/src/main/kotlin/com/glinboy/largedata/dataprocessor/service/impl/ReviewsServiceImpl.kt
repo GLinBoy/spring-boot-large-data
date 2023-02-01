49e7ea1a5531b5a3a90fecb9dc391d0bb107fe49
GLinBoy <hojjatabedie@gmail.com>
2023-02-01 14:31:38 +0100

    👌 IMPROVE: Updated ReviewsServiceImpl to work with file

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/impl/ReviewsServiceImpl.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/impl/ReviewsServiceImpl.kt
@@ -1,22 +1,42 @@
 package com.glinboy.largedata.dataprocessor.service.impl
 
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.core.JsonToken
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.glinboy.largedata.dataprocessor.client.DataProviderServiceApi
+import com.glinboy.largedata.dataprocessor.repository.ReviewRepository
 import com.glinboy.largedata.dataprocessor.service.ReviewsServiceApi
 import com.glinboy.largedata.dataprocessor.service.processor.ReviewHandler
+import com.glinboy.largedata.shared.dto.ReviewDTO
 import org.springframework.stereotype.Service
+import java.io.File
 
 @Service
 class ReviewsServiceImpl(
     private val dataProviderServiceApi: DataProviderServiceApi,
-    private val reviewHandler: ReviewHandler
+    private val reviewHandler: ReviewHandler,
+    private val om: ObjectMapper,
+    private val repository: ReviewRepository
 ): ReviewsServiceApi {
+
+    private val jf: JsonFactory = JsonFactory()
     override fun fetchSampleReviews() {
-        val reviewDTOs = dataProviderServiceApi.getSampleData()
-        reviewHandler.getReviewHandlerChain().process(reviewDTOs)
+        repository.deleteAllInBatch()
+        processFile(dataProviderServiceApi.getSampleData())
     }
 
     override fun fetchAllData() {
-        val reviewDTOs = dataProviderServiceApi.getAllData()
-        reviewHandler.getReviewHandlerChain().process(reviewDTOs)
+        repository.deleteAllInBatch()
+        processFile(dataProviderServiceApi.getAllData())
+    }
+
+    private fun processFile(file: File) {
+        jf.createParser(file).use {
+            it.nextToken()
+            while (it.nextToken() != JsonToken.END_ARRAY) {
+                val reviewDTO: ReviewDTO = om.readValue(it, ReviewDTO::class.java)
+                reviewHandler.getReviewHandlerChain().process(reviewDTO)
+            }
+        }
     }
 }
