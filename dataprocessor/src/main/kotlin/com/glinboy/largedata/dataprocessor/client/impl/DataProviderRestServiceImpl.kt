786b001f63cf0da6f2a55d573359e9a7b509fa28
GLinBoy <hojjatabedie@gmail.com>
2023-02-01 14:30:21 +0100

    👌 IMPROVE: Updated client impl to return file

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/client/impl/DataProviderRestServiceImpl.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/client/impl/DataProviderRestServiceImpl.kt
@@ -1,18 +1,17 @@
 package com.glinboy.largedata.dataprocessor.client.impl
 
 import com.glinboy.largedata.dataprocessor.client.DataProviderServiceApi
-import com.glinboy.largedata.shared.dto.ReviewDTO
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
+import org.springframework.util.StreamUtils
 import org.springframework.web.client.RestTemplate
+import java.io.File
+import java.io.FileOutputStream
 
 @Service
 class DataProviderRestServiceImpl: DataProviderServiceApi {
 
-    private val log = LoggerFactory.getLogger(javaClass)
     private val rt = RestTemplate()
 
     @Value("\${application.data-provider-url.sample}")
@@ -21,17 +20,21 @@ class DataProviderRestServiceImpl: DataProviderServiceApi {
     @Value("\${application.data-provider-url.all}")
     lateinit var dataUrl: String
 
-    override fun getSampleData(): List<ReviewDTO> {
-        val response = rt.exchange(dataSampleUrl, HttpMethod.GET, null,
-            object : ParameterizedTypeReference<List<ReviewDTO>>() {})
-        log.info("Status code: ${response.statusCode}, Data count: ${response.body!!.size}")
-        return response.body!!
+    override fun getSampleData(): File {
+        return getResultAsFile(dataSampleUrl)
     }
 
-    override fun getAllData(): List<ReviewDTO> {
-        val response = rt.exchange(dataUrl, HttpMethod.GET, null,
-            object : ParameterizedTypeReference<List<ReviewDTO>>() {})
-        log.info("Status code: ${response.statusCode}, Data count: ${response.body!!.size}")
-        return response.body!!
+    override fun getAllData(): File {
+        return getResultAsFile(dataUrl)
     }
+
+    private fun getResultAsFile(url: String): File = rt.execute(
+        url,
+        HttpMethod.GET,
+        null,
+        { clientHttpResponse ->
+            val f: File = File.createTempFile("download_", "_tmp")
+            StreamUtils.copy(clientHttpResponse.body, FileOutputStream(f))
+            f
+        })!!
 }
