618e9366f836bee14dbf0be7ebdfcc667e33d9d3
GLinBoy <hojjatabedie@gmail.com>
2023-02-01 14:29:09 +0100

    👌 IMPROVE: Deleted all unnecessary code

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/RunMe.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/RunMe.kt
@@ -1,39 +1,17 @@
 package com.glinboy.largedata.dataprocessor
 
-import com.glinboy.largedata.dataprocessor.client.DataProviderServiceApi
-import com.glinboy.largedata.shared.dto.ReviewDTO
-import com.glinboy.largedata.dataprocessor.entity.Review
-import com.glinboy.largedata.dataprocessor.repository.ReviewRepository
 import org.slf4j.LoggerFactory
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
 
-//@Component
-class RunMe(
-    private val dataProviderServiceApi: DataProviderServiceApi,
-    private val reviewRepository: ReviewRepository
-) {
+@Component
+class RunMe {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @EventListener(ApplicationReadyEvent::class)
     fun run() {
         logger.info("Let's do some logic!")
-        val all: List<ReviewDTO> = dataProviderServiceApi.getSampleData()
-        logger.info("---> Data count: {}", all.count())
-        reviewRepository.saveAll(
-            all.map {
-                Review(
-                    null,
-                    it.reviewDate,
-                    it.movieId,
-                    it.userId,
-                    it.isSpoiler,
-                    it.reviewText,
-                    it.rating,
-                    it.reviewSummary
-                )
-            }
-        )
     }
 }
