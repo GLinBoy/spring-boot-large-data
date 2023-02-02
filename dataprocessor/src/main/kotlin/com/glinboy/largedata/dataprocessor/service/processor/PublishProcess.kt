17383e7da416e0af9325f37cfebec1cb50b69450
GLinBoy <hojjatabedie@gmail.com>
2023-02-02 09:56:10 +0100

    👌 IMPROVE: Updated PublishProcess to work with one item

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
@@ -6,15 +6,13 @@ import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
 @Component
-class PublishProcess(private val kafkaTemplate: KafkaTemplate<Object, ReviewDTO>):
-    AbstractProcess<List<ReviewDTO>>() {
+class PublishProcess(private val kafkaTemplate: KafkaTemplate<String, ReviewDTO>) :
+    AbstractProcess<ReviewDTO>() {
 
     @Value("\${application.kafka-topic}")
     lateinit var topic: String
 
-    override fun job(reviewDTOs: List<ReviewDTO>) {
-        reviewDTOs.map {
-            kafkaTemplate.send(topic, it)
-        }
+    override fun job(reviewDTO: ReviewDTO) {
+        kafkaTemplate.send(topic, reviewDTO)
     }
 }
