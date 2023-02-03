1619fa435eb6597b5a6d7fef7708c7ecc1f5c7a5
GLinBoy <hojjatabedie@gmail.com>
2023-02-03 09:58:25 +0100

    👌 IMPROVE: Load KafkaConfig on dev profile/env

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/config/KafkaConfig.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/config/KafkaConfig.kt
@@ -2,8 +2,10 @@ package com.glinboy.largedata.dataprocessor.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.kafka.test.EmbeddedKafkaBroker
 
+@Profile("dev")
 @Configuration
 class KafkaConfig {
 
