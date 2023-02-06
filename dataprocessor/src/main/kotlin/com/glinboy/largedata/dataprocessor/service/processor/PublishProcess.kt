22caf4fbe4b0efa3ce5667543d61a5bdd3fcccee
GLinBoy <hojjatabedie@gmail.com>
2023-02-06 10:31:29 +0100

    👌 IMPROVE: Converted PublishProcess to interface

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/PublishProcess.kt
@@ -1,18 +1,4 @@
 package com.glinboy.largedata.dataprocessor.service.processor
 
-import com.glinboy.largedata.shared.dto.ReviewDTO
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.stereotype.Component
-
-@Component
-class PublishProcess(private val kafkaTemplate: KafkaTemplate<String, ReviewDTO>) :
-    AbstractProcess<ReviewDTO>() {
-
-    @Value("\${application.kafka-topic}")
-    lateinit var topic: String
-
-    override fun job(reviewDTO: ReviewDTO) {
-        kafkaTemplate.send(topic, reviewDTO)
-    }
+interface PublishProcess<T> : Process<T> {
 }
