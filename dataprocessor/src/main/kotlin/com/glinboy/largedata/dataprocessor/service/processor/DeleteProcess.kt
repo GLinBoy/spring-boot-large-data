470f22534265c9076f192041f4fee9f83443b237
GLinBoy <hojjatabedie@gmail.com>
2023-02-06 10:29:26 +0100

    👌 IMPROVE: Updated DeleteProcess to the Interface

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/DeleteProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/DeleteProcess.kt
@@ -1,13 +1,4 @@
 package com.glinboy.largedata.dataprocessor.service.processor
 
-import com.glinboy.largedata.dataprocessor.repository.ReviewRepository
-import com.glinboy.largedata.shared.dto.ReviewDTO
-import org.springframework.stereotype.Component
-
-@Component
-class DeleteProcess(private val reviewRepository: ReviewRepository) :
-    AbstractProcess<ReviewDTO>() {
-    override fun job(data: ReviewDTO) {
-        reviewRepository.deleteAllInBatch()
-    }
+interface DeleteProcess<T> : Process<T> {
 }
