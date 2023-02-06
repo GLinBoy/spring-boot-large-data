0e1b5ca4853ff96a1a92d65a9b2cc8e7e9131858
GLinBoy <hojjatabedie@gmail.com>
2023-02-06 10:33:25 +0100

    👌 IMPROVE: Converted SaveProcess to the interface

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/SaveProcess.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/SaveProcess.kt
@@ -1,25 +1,4 @@
 package com.glinboy.largedata.dataprocessor.service.processor
 
-import com.glinboy.largedata.dataprocessor.entity.Review
-import com.glinboy.largedata.dataprocessor.repository.ReviewRepository
-import com.glinboy.largedata.shared.dto.ReviewDTO
-import org.springframework.stereotype.Component
-
-@Component
-class SaveProcess(private val reviewRepository: ReviewRepository) : AbstractProcess<ReviewDTO>() {
-
-    override fun job(reviewDTO: ReviewDTO) {
-        reviewRepository.save(
-            Review(
-                null,
-                reviewDTO.reviewDate,
-                reviewDTO.movieId,
-                reviewDTO.userId,
-                reviewDTO.isSpoiler,
-                reviewDTO.reviewText,
-                reviewDTO.rating,
-                reviewDTO.reviewSummary
-            )
-        )
-    }
+interface SaveProcess<T> : Process<T> {
 }
