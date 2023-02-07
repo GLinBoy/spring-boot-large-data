c5890d9cf7baff0fb65d852138786d5caf4319e3
GLinBoy <hojjatabedie@gmail.com>
2023-02-07 11:52:36 +0100

    📦 NEW: Added a memory efficient for review service

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/impl/ReviewsServiceImpl.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/impl/ReviewsServiceImpl.kt
@@ -6,15 +6,18 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.glinboy.largedata.dataprocessor.client.DataProviderServiceApi
 import com.glinboy.largedata.dataprocessor.repository.ReviewRepository
 import com.glinboy.largedata.dataprocessor.service.ReviewsServiceApi
+import com.glinboy.largedata.dataprocessor.service.processor.Process
 import com.glinboy.largedata.dataprocessor.service.processor.ReviewHandler
 import com.glinboy.largedata.shared.dto.ReviewDTO
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 import java.io.File
 
 @Service
+@ConditionalOnProperty(value = ["application.mode"], havingValue = "memory-efficient")
 class ReviewsServiceImpl(
-    private val dataProviderServiceApi: DataProviderServiceApi,
-    private val reviewHandler: ReviewHandler,
+    private val dataProviderServiceApi: DataProviderServiceApi<File>,
+    private val reviewHandler: ReviewHandler<Process<ReviewDTO>, ReviewDTO>,
     private val om: ObjectMapper,
     private val repository: ReviewRepository
 ): ReviewsServiceApi {
