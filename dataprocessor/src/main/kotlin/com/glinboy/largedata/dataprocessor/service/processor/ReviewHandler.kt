c66e2234208e44fc0e202aca27f46fb435bba8f5
GLinBoy <hojjatabedie@gmail.com>
2023-02-07 11:50:50 +0100

    👌 IMPROVE: Converted ReviewHandler to the interface

--- dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/ReviewHandler.kt
+++ dataprocessor/src/main/kotlin/com/glinboy/largedata/dataprocessor/service/processor/ReviewHandler.kt
@@ -1,15 +1,5 @@
 package com.glinboy.largedata.dataprocessor.service.processor
 
-import com.glinboy.largedata.shared.dto.ReviewDTO
-import org.springframework.stereotype.Component
-
-@Component
-class ReviewHandler(
-    private val saveProcess: SaveProcess,
-    private val publishProcess: PublishProcess
-) {
-    fun getReviewHandlerChain(): AbstractProcess<ReviewDTO> {
-        saveProcess.setNext(publishProcess)
-        return saveProcess
-    }
+interface ReviewHandler<T : Process<E>, E> {
+    fun getReviewHandlerChain(): T
 }
