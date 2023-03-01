6dd18df6eb61445f3a0e1d9a0461ab234ca774d8
GLinBoy <hojjatabedie@gmail.com>
2023-03-01 20:31:55 +0100

    👌 IMPROVE: Upgraded dependency ben-manes [0.44.0 -> 0.46.0]
    #106

--- dataprocessor/build.gradle.kts
+++ dataprocessor/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 	kotlin("plugin.jpa") version "1.7.22"
-	id("com.github.ben-manes.versions") version "0.44.0"
+	id("com.github.ben-manes.versions") version "0.46.0"
     id("org.ec4j.editorconfig") version "0.0.3"
 }
 
