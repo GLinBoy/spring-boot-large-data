209b185018489bd6e9a51bd67583787968dfb207
GLinBoy <hojjatabedie@gmail.com>
2023-08-08 19:37:47 +0200

    👌 IMPROVE: Upgraded Kotlin of the data provider to 1.8.22
    resolved #146

--- dataprocessor/build.gradle.kts
+++ dataprocessor/build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.1.2"
 	id("io.spring.dependency-management") version "1.1.2"
-	kotlin("jvm") version "1.8.21"
-	kotlin("plugin.spring") version "1.8.21"
-	kotlin("plugin.jpa") version "1.8.21"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+	kotlin("plugin.jpa") version "1.8.22"
 	id("com.github.ben-manes.versions") version "0.46.0"
     id("org.ec4j.editorconfig") version "0.0.3"
 }
