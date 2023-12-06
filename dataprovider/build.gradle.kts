d0f95c5648a4ab45bdfe84351f11d38d9c26b56b
GLinBoy <hojjatabedie@gmail.com>
2023-12-06 18:40:48 +0100

    👌 IMPROVE: Upgraded Kotlin to 1.9.20
    resolved #168

--- dataprovider/build.gradle.kts
+++ dataprovider/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
+	kotlin("jvm") version "1.9.20"
+	kotlin("plugin.spring") version "1.9.20"
 }
 
 group = "com.glinboy.largedata"
