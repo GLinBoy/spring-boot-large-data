2f3410f0f4184f2b7cf57650d1d1450eea67a13e
GLinBoy <hojjatabedie@gmail.com>
2023-12-06 18:44:35 +0100

    👌 IMPROVE: Upgraded Spring Boot of data processor to 3.2.0
    resolved #171

--- dataprocessor/build.gradle.kts
+++ dataprocessor/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.5"
-	id("io.spring.dependency-management") version "1.1.3"
+	id("org.springframework.boot") version "3.2.0"
+	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
 	kotlin("plugin.jpa") version "1.8.22"
