17d23c23707479c371f2157425794fbe4c697c52
GLinBoy <hojjatabedie@gmail.com>
2023-12-06 18:41:16 +0100

    👌 IMPROVE: Upgraded Java 21
    resolved #169

--- dataprovider/build.gradle.kts
+++ dataprovider/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.glinboy.largedata"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
 	mavenCentral()
@@ -27,7 +27,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = "21"
 	}
 }
 
