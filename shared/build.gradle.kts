92d7b00a41980280d6bf158c5083972d00276657
GLinBoy <hojjatabedie@gmail.com>
2023-12-07 20:33:42 +0100

    👌 IMPROVE: Upgraded java of shared to 21
    resolved #177

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.glinboy.largedata"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
 	mavenCentral()
@@ -19,7 +19,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = "21"
 	}
 }
 
