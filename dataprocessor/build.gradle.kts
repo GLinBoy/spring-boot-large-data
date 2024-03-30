14fb61a47d4913a02b5e9928b9ed774d4eaeb872
GLinBoy <glinboy@live.com>
2024-03-30 13:56:27 +0100

    👌 IMPROVE: Upgraded Kotlin of data processor to 1.9.23
    resolved #218

--- dataprocessor/build.gradle.kts
+++ dataprocessor/build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
-	kotlin("plugin.jpa") version "1.9.22"
+	kotlin("jvm") version "1.9.23"
+	kotlin("plugin.spring") version "1.9.23"
+	kotlin("plugin.jpa") version "1.9.23"
 	id("com.github.ben-manes.versions") version "0.50.0"
     id("org.ec4j.editorconfig") version "0.0.3"
 }
