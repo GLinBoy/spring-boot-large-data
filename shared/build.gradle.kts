fecdfcff96716ad5fb666a9bb2dbd2ad8d059277
GLinBoy <glinboy@live.com>
2024-03-30 13:26:54 +0100

    👌 IMPROVE: Upgraded Java of the shared to 22
    resolved #209

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.glinboy.largedata"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_21
+java.sourceCompatibility = JavaVersion.VERSION_22
 
 repositories {
 	mavenCentral()
@@ -19,7 +19,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "21"
+		jvmTarget = "22"
 	}
 }
 
