80e2a1118a499bc0149b93e44084f9866b36ef32
GLinBoy <glinboy@live.com>
2024-05-27 17:48:58 +0200

    👌 IMPROVE: Upgraded Spring Boot of the data provider module to 3.3.0
    resolved #225

--- dataprovider/build.gradle.kts
+++ dataprovider/build.gradle.kts
@@ -1,15 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.5"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("org.springframework.boot") version "3.3.0"
+	id("io.spring.dependency-management") version "1.1.5"
 	kotlin("jvm") version "1.9.23"
 	kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "com.glinboy.largedata"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_22
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
 	mavenCentral()
@@ -27,7 +27,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "22"
+		jvmTarget = "21"
 	}
 }
 
