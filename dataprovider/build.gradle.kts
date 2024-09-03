8ff31226ebd08eedf2a67875619c67d3112c944f
GLinBoy <glinboy@live.com>
2024-09-03 20:32:35 +0200

    👌 IMPROVE: Upgrade Kotlin of data provider to 1.9.25
    resolved #251

--- dataprovider/build.gradle.kts
+++ dataprovider/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
 }
 
 group = "com.glinboy.largedata"
