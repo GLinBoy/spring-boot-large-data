467d95d425a685e1f07c25bf9da7361beb9acb60
GLinBoy <glinboy@live.com>
2025-01-03 17:05:27 +0100

    👌 IMPROVE: Upgrade Spring Boot of the data processor to 3.4.1
    resolved #275

--- dataprovider/build.gradle.kts
+++ dataprovider/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.4.0"
-	id("io.spring.dependency-management") version "1.1.6"
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
 }
