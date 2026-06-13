fc295ed0f3726795a83484aee2adfb25567aef1a
GLinBoy <glinboy@live.com>
2026-06-13 15:38:04 +0200

    👌 IMPROVE: Upgrade Kotlin of data provider to 2.3.21

--- dataprovider/build.gradle.kts
+++ dataprovider/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
 	id("org.springframework.boot") version "4.1.0"
 	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
+	kotlin("jvm") version "2.3.21"
+	kotlin("plugin.spring") version "2.3.21"
 }
 
 group = "com.glinboy.largedata"
